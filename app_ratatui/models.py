1fa686455b8deff7c2934e49f72b6a033da3cc8e
vladt <vladtihonov1183@gmail.com>
2025-01-13 17:55:54 +0100

    yui

--- app_ratatui/models.py
+++ app_ratatui/models.py
@@ -13,3 +13,6 @@ class ProjectsModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.NamesProjects}//////{self.created_at}"
